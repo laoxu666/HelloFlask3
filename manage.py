f8ee268ff5d2817a18d70ffa378107abcebb079c
laoxu <xsmlxh666@163.com>
2018-07-19 22:03:59 +0800

    flask 简单操作

--- manage.py
+++ manage.py
@@ -2,13 +2,15 @@ from flask import Flask
 from flask_script import Manager
 
 from APP.views import blue
-
+from APP.views2 import blue as blue2
 app = Flask(__name__)
 
 manager = Manager(app=app)
 
 # 注册蓝图
 app.register_blueprint(blueprint=blue)
+
+app.register_blueprint(blueprint=blue2)
 
 if __name__ == '__main__':
     # APP.run()
